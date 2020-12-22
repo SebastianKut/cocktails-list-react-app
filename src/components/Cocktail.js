c34ecff179022367edc52cfaeb50eef65302660f
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-12-22 08:34:38 +0100

    fetch done

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context';
 
-const Cocktail = () => {
+const Cocktail = ({ id, glass, image, info, name }) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
-}
+    <article className="cocktail">
+      <div className="image-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
+          Details
+        </Link>
+      </div>
+    </article>
+  );
+};
 
-export default Cocktail
+export default Cocktail;
